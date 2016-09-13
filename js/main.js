f3387cb2fd2bc6b6c78eb559b500563a98871dfa
titli1981 <madhumishra084@gmailcom>
2016-09-13 16:20:44 -0400

    click listner,switched the classes and changed the status text

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 
+$(document).ready(function() {
+    console.log( "ready!");
 
+
+    $("button").click(function(){
+        if ($("button").hasClass("on")){
+        $("button").addClass("off").removeClass("on")
+        $("body").addClass("dark").removeClass("light")
+        $(".status").text("Who turned off the light")
+    }
+
+        else
+        {$("button").addClass("on").removeClass("off")
+        $("body").addClass("light").removeClass("dark")
+        $(".status").text("It's so bright in here!")
+    }
+
+    });
+
+})
